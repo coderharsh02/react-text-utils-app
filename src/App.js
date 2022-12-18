e9bda9f93e4ed461fc7256db2481ed273980dec4
Harsh Shah <harshshah.coder@gmail.com>
2022-12-18 20:43:04 +0530

    changed BrowserRouter to HashRouter for gitHub pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
